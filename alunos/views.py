25506ceea0cb0bfc2ca2ff534ff637de17c3d77f
“mtstechnologies” <michael.trindade@hotmail.com>
2025-02-13 19:15:53 -0400

    Chamando template HTML.

--- alunos/views.py
+++ alunos/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def criar_aluno(request):
-    return HttpResponse('Ola mundo!')
+    return render(request, 'criar_aluno.html')
 
 def listar_aluno(request):
     return HttpResponse('Estou listando os alunos')
