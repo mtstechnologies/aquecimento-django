d54e9ea377e7ec8e5f0c18ff41c6544c769168c5
“mtstechnologies” <michael.trindade@hotmail.com>
2025-02-15 16:21:55 -0400

    Redirecionando para pagina de cadasreo

--- alunos/views.py
+++ alunos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Aluno
 
@@ -7,20 +7,24 @@ def criar_aluno(request):
     if request.method == 'GET':
         return render(request, 'criar_aluno.html')
     elif request.method == 'POST':
-        #é assim que se pega os dados do formulario para salvar no banco
+        # é assim que se pega os dados do formulario para salvar no banco
         nome = request.POST.get('nome')
         idade = request.POST.get('idade')
         email = request.POST.get('email')
 
-        #criando uma instancia da classe modelo, e suas variaveis recebem o que vem do formulario
+        # validando os dados do formulario antes de serem salvos
+        '''if len(nome.strip()) == 0 or idade < 0:
+            ...'''
+    
+        # criando uma instancia da classe modelo, e suas variaveis recebem o que vem do formulario
         aluno = Aluno(
-            nome = nome,
-            idade = idade,
-            email = email
+            nome=nome,
+            idade=idade,
+            email=email
         )
         aluno.save()
-        
-        return HttpResponse('Cadastro realizado')
+        #redirecionando a pagina apos cadastro
+        return redirect('criar_aluno')
 
 
 def listar_aluno(request):
