36ab785a0dea2377fdbb6ec675c506fb05a51fc5
“mtstechnologies” <michael.trindade@hotmail.com>
2025-02-15 13:52:38 -0400

    cadastro obter os dados enviados no formulario e salvando no banco

--- alunos/urls.py
+++ alunos/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('criar_aluno/', views.criar_aluno),
+    path('criar_aluno/', views.criar_aluno, name="criar_aluno"),
     path('listar_aluno/', views.listar_aluno),
 ]
--- alunos/views.py
+++ alunos/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Aluno
+
 
 def criar_aluno(request):
-    return render(request, 'criar_aluno.html')
+    if request.method == 'GET':
+        return render(request, 'criar_aluno.html')
+    elif request.method == 'POST':
+        #é assim que se pega os dados do formulario para salvar no banco
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+        email = request.POST.get('email')
+
+        #criando uma instancia da classe modelo, e suas variaveis recebem o que vem do formulario
+        aluno = Aluno(
+            nome = nome,
+            idade = idade,
+            email = email
+        )
+        aluno.save()
+        
+        return HttpResponse('Cadastro realizado')
+
 
 def listar_aluno(request):
     return HttpResponse('Estou listando os alunos')
 
 
-
-
-
-
+# capturar os dados enviados no formulario e salvar no banco
